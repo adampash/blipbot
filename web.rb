daac1fb024a4838d48d1571db6f099085682142e
Adam Pash <adam.pash@gmail.com>
2015-02-24 10:47:56 -0500

    logging params
    updated for curl work
    
    again
    
    one more time
    
    yeah
    
    again
    
    ugh

--- web.rb
+++ web.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
 require 'kinja'
+require 'json'
+require_relative './lib/post_client'
 
 client = Kinja::Client.new(
   user: ENV["KINJA_USER"],
@@ -11,4 +13,18 @@ post '/:id' do
     headline: 'foo',
     body: 'bar'
   )
+  status 200
+  body ''
+end
+
+post '/' do
+  puts params
+  url = params[:url]
+  post_json = PostClient.get_post_json(url)
+  post = client.post(
+    headline: '',
+    body: PostClient.format_body(post_json)
+  )
+  status 200
+  body ''
 end
