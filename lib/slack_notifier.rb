6d488c6d78768493f17ef652b6f3b143bd6e94d7
Adam Pash <adam.pash@gmail.com>
2015-03-09 14:02:06 -0400

    test

--- lib/slack_notifier.rb
+++ lib/slack_notifier.rb
@@ -2,8 +2,8 @@ SLACK_TOKEN = ENV["SLACK_TOKEN"]
 SLACK_WEBHOOK = ENV["SLACK_WEBHOOK"]
 
 module SlackNotifier
-  def self.notify(message, channel="#editlead")
-    notifier = Slack::Notifier.new SLACK_WEBHOOK
+  def self.notify(message, channel="#labs-test")
+    notifier = Slack::Notifier.new SLACK_WEBHOOK, {"unfurl_links": false}
 
     notifier.ping message,
       icon_emoji: ":chart_with_upwards_trend:",
